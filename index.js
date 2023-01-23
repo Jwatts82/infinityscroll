ff98530c8177a4045a7ddaf6664867146dff8f99
Jwatts82 <wattscodes@gmail.com>
2023-01-23 15:29:40 -0500

    fetch data

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const count = 10
-const apiKey = config.API_KEY
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+const apiKey = config.MY_KEY
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=pE-JhLaW8c0ehzthFKwLlBCudwxlpUN4PwfL2Ji4YFI&count=${count}`
+
+async function getPhotos() {
+  const res = await fetch(apiURL)
+  const data = res.json()
+
+  console.log(data)
+}
+
+
+getPhotos()
